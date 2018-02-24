2b321430588477958202079a72456a5e4606b939
marisbotero <maris2143@gmail.com>
2018-02-24 18:34:57 -0500

    Fix

--- circle.R
+++ circle.R
@@ -1,22 +1,20 @@
-is.circle = function(circle_x, circle_y, r, x, y){
+#True = dentro de la circunsferencia
+#False = Fuera de la circunsferencia
+#center_x = coordenanda en X
+#center_y = coordenada en y
+# r por defecto 1
 
-  
+is.circle = function(center_x, center_y, x, y, r) {
+
   if ( r != 1 )
   {stop ("El radio por defecto debe ser 1.")}
-  if (circle_x != 0)
-  {stop("El centro debe ser el origen de las cordenadas")}
-  if (circle_y != 0)
-  {stop("El centro debe ser el origen de las cordenadas")}
 
-
-  
-  d = ((circle_x - x)^2 + (circle_y - y)^2)
-  if (d < r^2){
+  d = r^2 - (center_x-x)^2 + (center_y-y)^2
+  if (d < 0){
     "True"
   }
-  
   else{
     "False"
   }
-
 }
+
